ab3bdccb7b7c6a8cf9fd178439713c25a1f6904d
GabrielMBatista <gabbriel_gbl2@hotmail.com>
2021-01-20 11:53:37 -0300

    criada função basica de botoes e troca entre paginas
    botoes venda e compra entram na pagina e se clicados novamente voltam para home.
    nome futurecar ao ser clicado tambem leva para a home.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,73 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
-import { MainContainer} from './components/StyledSellers'
+import { ProductPage } from './components/ProductPage'
+import { SellersPage } from './components/SellersPage'
+import { Main } from './components/Main'
+
+
+export const MainContainer = styled.div`
+display: grid;
+grid-template-rows: 15% 70% 15%;
+flex-direction: column;
+max-width: 100vw;
+height: 100vh;
+align-items:center;
+justify-content:center;
+`
+
 
 export class App extends Component {
+	state = {
+		ProductPage: false,
+		SellersPage: false,
+	}
+
+	ChangePageSell = () => {
+		this.setState({
+			SellersPage: !this.state.SellersPage
+		})
+		this.setState({
+			ProductPage: false
+		});
+	  }
+	  ChangePageBuy = () => {
+		this.setState({
+			ProductPage: !this.state.ProductPage
+		})
+		this.setState({
+			SellersPage: false
+		});
+	  }
+	  ChangePageHome = () => {
+		this.setState({
+			ProductPage: false,
+			SellersPage: false
+		})
+	  }
+	ShowPage = () => {
+		if (this.state.ProductPage) {
+			return (
+				<ProductPage />
+			)
+		}
+		else if (this.state.SellersPage) {
+			return (
+				<SellersPage />
+			)
+		}
+
+		return (
+			<Main/>
+		)
+	}
 	render() {
 		return (
 			<MainContainer>
-				<Header></Header>
-
-				<Footer></Footer>
+				<Header buy={this.ChangePageBuy} sell={this.ChangePageSell}home={this.ChangePageHome}/>
+				{this.ShowPage()}
+				<Footer/>
 			</MainContainer>
 		)
 	}
